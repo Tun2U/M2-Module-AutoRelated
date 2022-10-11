4d03c9b2184d8fa76d0ccfe72f9d70cd4f5bfeef
Enrico Falappa <e.falappa@tun2u.com>
2022-10-11 11:45:04 +0200

    v1.0.5 optional data

--- Block/Product/ProductList/Related.php
+++ Block/Product/ProductList/Related.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @category    Tun2U
  * @package     Tun2U_AutoRelated
@@ -15,102 +16,101 @@ use Tun2U\AutoRelated\Model\Collection;
 class Related extends \Magento\Catalog\Block\Product\ProductList\Related
 {
 
-    /**
-    * @var \Magento\Core\Model\Factory\Helper
-    */
-    protected $_helper;
+	/**
+	 * @var \Magento\Core\Model\Factory\Helper
+	 */
+	protected $_helper;
 
 
-    /**
-    * @var \Magento\Store\Model\StoreManagerInterface
-    */
-    protected $_storeManager;
+	/**
+	 * @var \Magento\Store\Model\StoreManagerInterface
+	 */
+	protected $_storeManager;
 
 
-    /**
-    * @var \Magento\Framework\Registry
-    */
-    protected $_registry;
+	/**
+	 * @var \Magento\Framework\Registry
+	 */
+	protected $_registry;
 
 
-    /**
-    * @var \Tun2U\AutoRelated\Model\Collection
-    */
-    protected $_collection;
+	/**
+	 * @var \Tun2U\AutoRelated\Model\Collection
+	 */
+	protected $_collection;
 
 
-    public function __construct(
-        \Magento\Catalog\Block\Product\Context $context,
-        \Magento\Checkout\Model\ResourceModel\Cart $checkoutCart,
-        \Magento\Catalog\Model\Product\Visibility $catalogProductVisibility,
-        \Magento\Checkout\Model\Session $checkoutSession,
-        \Magento\Framework\Module\Manager $moduleManager,
-        array $data = [],
-        ObjectManagerInterface $helperFactory,
-        Collection $collection
-    ) {
-        $this->_helper = $helperFactory;
-        $this->_registry = $context->getRegistry();
-        $this->_storeManager = $context->getStoreManager();
-        ;
-        $this->_collection = $collection;
-        parent::__construct(
-            $context,
-            $checkoutCart,
-            $catalogProductVisibility,
-            $checkoutSession,
-            $moduleManager,
-            $data
-        );
+	public function __construct(
+		\Magento\Catalog\Block\Product\Context $context,
+		\Magento\Checkout\Model\ResourceModel\Cart $checkoutCart,
+		\Magento\Catalog\Model\Product\Visibility $catalogProductVisibility,
+		\Magento\Checkout\Model\Session $checkoutSession,
+		\Magento\Framework\Module\Manager $moduleManager,
+		ObjectManagerInterface $helperFactory,
+		Collection $collection,
+		array $data = []
+	) {
+		$this->_helper = $helperFactory;
+		$this->_registry = $context->getRegistry();
+		$this->_storeManager = $context->getStoreManager();;
+		$this->_collection = $collection;
+		parent::__construct(
+			$context,
+			$checkoutCart,
+			$catalogProductVisibility,
+			$checkoutSession,
+			$moduleManager,
+			$data
+		);
 
-        // Only cache if we have something thats keyable..
-        $_time = $this->_helper->get('Tun2U\AutoRelated\Helper\Data')->get_cache_lifetime();
+		// Only cache if we have something thats keyable..
+		$_time = $this->_helper->get('Tun2U\AutoRelated\Helper\Data')->get_cache_lifetime();
 
-        if ($_time > 0 && $cacheKey = $this->_cacheKey()) {
-            $this->addData(array(
-                'cache_lifetime'    => $_time,
-                'cache_tags'        => array(\Magento\Store\Model\Store::CACHE_TAG),
-                'cache_key'         => $cacheKey,
-            ));
-        }
-    }
+		if ($_time > 0 && $cacheKey = $this->_cacheKey()) {
+			$this->addData(array(
+				'cache_lifetime'    => $_time,
+				'cache_tags'        => array(\Magento\Store\Model\Store::CACHE_TAG),
+				'cache_key'         => $cacheKey,
+			));
+		}
+	}
 
-    protected function _cacheKey()
-    {
-        $product = $this->_registry->registry('product');
+	protected function _cacheKey()
+	{
+		$product = $this->_registry->registry('product');
 
-        if ($product) {
-            return get_class() . '::' .  $this->_storeManager->getStore()->getCode() . '::' . $product->getId();
-        }
+		if ($product) {
+			return get_class() . '::' .  $this->_storeManager->getStore()->getCode() . '::' . $product->getId();
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    protected function _prepareData()
-    {
-        parent::_prepareData();
+	protected function _prepareData()
+	{
+		parent::_prepareData();
 
-        $_enabled = $this->_helper->get('Tun2U\AutoRelated\Helper\Data')->get_enabled();
+		$_enabled = $this->_helper->get('Tun2U\AutoRelated\Helper\Data')->get_enabled();
 
-        if ($_enabled && count($this->getItems()) == 0) {
-            $_products = $this->_collection->getRelatedProducts();
-            if ($_products) {
-                $this->_itemCollection = $_products;
-            }
-        }
+		if ($_enabled && count($this->getItems()) == 0) {
+			$_products = $this->_collection->getRelatedProducts();
+			if ($_products) {
+				$this->_itemCollection = $_products;
+			}
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getIdentities()
-    {
-        $identities = [];
-       //Bugfix: Warning: Invalid argument supplied for foreach()
-        if (is_array($this->getItems()) || is_object($this->getItems())) {
-            foreach ($this->getItems() as $item) {
-                $identities = array_merge($identities, $item->getIdentities());
-            }
-        }
-        return $identities;
-    }
+	public function getIdentities()
+	{
+		$identities = [];
+		//Bugfix: Warning: Invalid argument supplied for foreach()
+		if (is_array($this->getItems()) || is_object($this->getItems())) {
+			foreach ($this->getItems() as $item) {
+				$identities = array_merge($identities, $item->getIdentities());
+			}
+		}
+		return $identities;
+	}
 }
